cc508721f059d16064748ebbee72eb2da39cb97c
n0tx <rcandra91@msn.com>
2024-12-07 02:41:33 +0700

    Initialize commit

---
+++ src/main/java/com/riki/hello/HelloRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.riki.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloRailwayApplication.class, args);
+	}
+
+}
